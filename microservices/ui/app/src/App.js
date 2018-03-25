294250521dd363968ba3a0d164ca43de3ddc54fa
Anna <annaoh1994@gmail.com>
2018-03-25 11:08:31 -0700

    Fix merge conflicts on Game Lobby

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -8,17 +8,7 @@ class App extends Component {
     // const clusterName = process.env.REACT_APP_CLUSTER_NAME || "NoClusterName";
     return (
       <div className="App">
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <GameLobby/>
-
       </div>
     );
   }
