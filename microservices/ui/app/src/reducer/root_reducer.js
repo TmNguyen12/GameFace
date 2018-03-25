b9ac04804eec6bbc44289a1b78394b14aa6eda18
Christine Song <christinedisong@gmail.com>
2018-03-25 11:40:33 -0700

    add user actions and user reducer to root reducer

--- microservices/ui/app/src/reducer/root_reducer.js
+++ microservices/ui/app/src/reducer/root_reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'; 
 import scenarioReducer from './scenario_reducer';
+import userReducer from './user_reducer'; 
 
 const rootReducer = combineReducers({
-    scenarios: scenarioReducer
+    scenarios: scenarioReducer,
+    users: userReducer
 }); 
 
 export default rootReducer;
+
