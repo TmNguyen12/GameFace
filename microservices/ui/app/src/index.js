6dd141a357f7ed38dad4369baac8f2f4c4cea77d
Truong Nguyen <tmnguyen12@gmail.com>
2018-03-24 22:27:49 -0700

    Update and create

--- microservices/ui/app/src/index.js
+++ microservices/ui/app/src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 //import registerServiceWorker from './registerServiceWorker';
 import { fetchScenario } from "./util/scenarios_util";
+import { createGame, fetchGame, updateGame } from "./util/games_util";
 
+window.updateGame = updateGame;
+window.createGame = createGame;
+window.fetchGame = fetchGame;
 window.fetchScenario = fetchScenario;
 ReactDOM.render(<App />, document.getElementById("root"));
